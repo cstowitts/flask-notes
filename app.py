25a349eb2d17e214ced6289801313ff90ea4d152
Cella Stowitts <78512719+cstowitts@users.noreply.github.com>
2022-02-08 17:33:51 -0800

    hides password when typing, handles sessions, updates all /secret routes to /user/username

--- app.py
+++ app.py
@@ -29,14 +29,17 @@ def show_registration_form():
         email, first_name, and last_name.
 
         POST: Process the registration form by adding a new user.
-        Then redirect to /secret
+        Then redirect to /users/<username>
     """
     register_form = RegistrationForm()
 
     if register_form.validate_on_submit():
-        data = {k: v for k, v in register_form.data.items() if k!= "csrf_token"}
-        #not hurting but we are expliticly passing in things
-        #could just do register_form.data
+        session.pop("username", None)
+
+        data = {k: v for k, v in register_form.data.items()
+                if k != "csrf_token"}
+        # not hurting but we are expliticly passing in things
+        # could just do register_form.data
 
         new_user = User.register(
                    data['username'],
@@ -45,9 +48,11 @@ def show_registration_form():
                    data['first_name'],
                    data['last_name'])
 
+        username = new_user.username
+        session["username"] = username
         db.session.add(new_user)
         db.session.commit()
-        return redirect('/secret')
+        return redirect(f'/users/{username}')
 
     else:
         return render_template("register.html", form=register_form)
@@ -58,31 +63,35 @@ def show_login_form():
     """GET: Show a form that when submitted will login a user.
         This form should accept a username and a password.
 
-        POST: Process the login form, 
-        authenticate user and if so, redirect to /secret
+        POST: Process the login form,
+        authenticate user and if so, redirect to /users/<username>
     """
 
     login_form = LoginForm()
 
+    session.pop("username", None)
+
     if login_form.validate_on_submit():
         username = login_form.username.data
         password = login_form.password.data
-        user = User.authenticate(username,password)
+        user = User.authenticate(username, password)
         if user:
-            session["username"] = user.username
-            return redirect("/secret")
+            username = user.username
+            session["username"] = username
+            return redirect(f'/users/{username}')
     else:
         return render_template("login.html", form=login_form)
 
 
-@app.get("/secret")
-def show_secret():
-    """Show secret page if user logged in"""
+@app.get("/users/<username>")
+def display_user_details(username):
+    """Display a template that shows information
+        about that user
+        (everything except for their password)"""
 
-    # if "username" not in session:
-    #     flash("You must be logged in to view")
-    #     return redirect("/register")
-    # else:
-    return render_template("secret.html")
-
-
+    if "username" not in session or session["username"] != username:
+        flash("You must be logged in to view")
+        return redirect("/register")
+    else:
+        user = User.query.filter_by(username=username).one_or_none()
+        return render_template("user.html", user=user)
--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 
-from wtforms import StringField
+from wtforms import StringField, PasswordField
 
-from wtforms.validators import InputRequired, Optional, Length
+from wtforms.validators import InputRequired, Length
 
 
 class RegistrationForm(FlaskForm):
@@ -10,14 +10,14 @@ class RegistrationForm(FlaskForm):
 
     username = StringField("Username",
                            validators=[InputRequired(), Length(max=20)])
-    password = StringField("Password",
-                           validators=[InputRequired(), Length(max=100)])
+    password = PasswordField("Password",
+                             validators=[InputRequired(), Length(max=100)])
     email = StringField("Email",
                         validators=[InputRequired(), Length(max=50)])
     first_name = StringField("First Name",
                              validators=[InputRequired(), Length(max=30)])
     last_name = StringField("Last Name",
-                             validators=[InputRequired(), Length(max=30)])
+                            validators=[InputRequired(), Length(max=30)])
 
 
 class LoginForm(FlaskForm):
@@ -25,5 +25,5 @@ class LoginForm(FlaskForm):
 
     username = StringField("Username",
                            validators=[InputRequired(), Length(max=20)])
-    password = StringField("Password",
-                           validators=[InputRequired(), Length(max=100)])
+    password = PasswordField("Password",
+                             validators=[InputRequired(), Length(max=100)])
